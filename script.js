090dd1d73c3d652a11b812ea267ff9bb3502550b
wanderleysoliveira <wanderley_santos@ymail.com>
2021-07-21 19:01:54 -0300

    Finalização do projeto

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let digitalElement = document.querySelector('.digital');
-let sElement = document.querySelector('p_s')
-let mElement = document.querySelector('p_m')
-let hElement = document.querySelector('p_h')
+let sElement = document.querySelector('.p_s')
+let mElement = document.querySelector('.p_m')
+let hElement = document.querySelector('.p_h')
 
 function updateClock(){
     let now = new Date();
@@ -11,17 +11,17 @@ function updateClock(){
 
     digitalElement.innerHTML = `${hour}:${fixZero(minute)}:${fixZero(second)}`
 
-    let sDeg = ((360 / 6) * second) - 90;
+    let sDeg = ((360 / 60) * second) - 90;
+    let mDeg = ((360 / 60 ) * minute) - 90;
+    let hDeg = ((360 / 12 ) * hour) - 90;
 
-    sElement.style.transform = `rotate(${sDeg}deg)`;    
-
-
+    sElement.style.transform = `rotate(${sDeg}deg)`;  
+    mElement.style.transform = `rotate(${mDeg}deg)`;  
+    hElement.style.transform = `rotate(${hDeg}deg)`;  
 }
 
 function fixZero(time) {
-
     return time < 10 ? `0${time}` : time;
-
     // if(time < 10) {
     //     return '0' + time;
     // }
@@ -31,3 +31,4 @@ function fixZero(time) {
 }
 
 setInterval(updateClock, 1000);
+updateClock();
