9e0d1e2f6dda7ea656d843f70ee24ca08fd51e9b
wanderleysoliveira <wanderley_santos@ymail.com>
2021-07-20 21:40:26 -0300

    Criação dos primeiros metodos do projeto

---
+++ script.js
@@ -0,0 +1,33 @@
+let digitalElement = document.querySelector('.digital');
+let sElement = document.querySelector('p_s')
+let mElement = document.querySelector('p_m')
+let hElement = document.querySelector('p_h')
+
+function updateClock(){
+    let now = new Date();
+    let hour = now.getHours();
+    let minute = now.getMinutes();
+    let second = now.getSeconds();
+
+    digitalElement.innerHTML = `${hour}:${fixZero(minute)}:${fixZero(second)}`
+
+    let sDeg = ((360 / 6) * second) - 90;
+
+    sElement.style.transform = `rotate(${sDeg}deg)`;    
+
+
+}
+
+function fixZero(time) {
+
+    return time < 10 ? `0${time}` : time;
+
+    // if(time < 10) {
+    //     return '0' + time;
+    // }
+    // else {
+    //     return time;
+    // }
+}
+
+setInterval(updateClock, 1000);
